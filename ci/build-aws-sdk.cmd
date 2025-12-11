8fb479bdc5781b43ec545a0b7ff35ba78cd0d9c6
Vladimir <summeroff@gmail.com>
2025-12-11 10:28:03 -0800

    fix memory dump upload (#131)
    * fix memory dump upload
    
    * revert yarn change

--- ci/build-aws-sdk.cmd
+++ ci/build-aws-sdk.cmd
@@ -1,5 +1,13 @@
 :: run before cmake of main project to build aws cpp sdk locally 
 :: it expect AWS_SDK_VERSION env variable to be set
+@echo off
+
+if not defined AWS_SDK_VERSION (
+    echo Error: AWS_SDK_VERSION environment variable is not set
+    exit /b 1
+)
+
+echo Building AWS SDK version: %AWS_SDK_VERSION%
 
 mkdir aws-sdk
 cd aws-sdk
@@ -10,7 +18,7 @@ mkdir build
 cd build
 
 cmake -G "Visual Studio 17 2022" -A x64 ^
--DBUILD_ONLY="s3;sts" ^
+-DBUILD_ONLY="s3;sts;transfer;cognito-identity;identity-management" ^
 -DENABLE_TESTING=OFF ^
 -DBUILD_SHARED_LIBS=OFF ^
 -DSTATIC_CRT=ON ^
